8e0ef1ec90fdf38e3f03768c7179855e58c01950
dkelley1215 <72112888+dkelley1215@users.noreply.github.com>
2020-11-22 15:51:38 -0600

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,19 @@
+public class ASCII {
+	public static void printNumbers()
+	  {
+	    // TODO: print valid numeric input
+		ASCII.printNumbers();
+	  }
+
+	  public static void printLowerCase()
+	  {
+	    // TODO: print valid lowercase alphabetic input
+		  ASCII.printLowerCase();
+	  }
+
+	  public static void printUpperCase()
+	  {
+	    // TODO: print valid uppercase alphabetic input
+		  ASCII.printUpperCase();
+	  }
+	}
